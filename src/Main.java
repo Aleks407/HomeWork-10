b4c5e7ca204b8c33d17af777e84d2638399b05da
Aleks <leksa407@gmail.com>
2024-09-22 21:48:50 +0300

    HW 10 на проверку

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//Task 1
+        System.out.println("Задача №1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName+' ' +firstName+' ' + middleName;
+
+
+        System.out.println("Ф. И. О. сотрудника — "+fullName);
+//Task 2
+        System.out.println("Задача №2");
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — "
+                +fullName.toUpperCase());
+//Task 3
+        System.out.println("Задача №3");
+        String fullName2 = "Иванов Семён Семёнович";
+        String result = fullName2.replace('ё','е');
+        System.out.println("Данные Ф. И. О. сотрудника - "+result);
+
     }
 }
